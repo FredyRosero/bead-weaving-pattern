6a77561a11f9421f9f76516fc5fa87c381a0d68e
FredyRosero <faroseroc@unal.edu.co>
2022-02-15 16:50:07 -0500

    initial run

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="preview">
+      <svg width="100" height="100">
+            <circle cx="5" cy="5" r="5" stroke="green" strokeWidth="4" fill="yellow" />
+          </svg>        
+      </div>
+      <div className="canvasContainer">
+        <div className="sides">
+        </div>
+        <div className="canvas">
+          <button>11</button>
+          <button>12</button>
+          <button>13</button>
+          <button>14</button>
+          <button>11</button>
+          <button>12</button>
+          <button>13</button>
+          <button>14</button>
+        </div>
+        <div className="sides">
+
+        </div>
+      </div>
+      <div className="picker">
+        <button className="left">&#60;</button>
+        <button>12</button>
+        <button>13</button>
+        <button>14</button>
+        <button className="right">&#62;</button>
+        <button className="more">+</button>
+      </div>
     </div>
   );
 }
